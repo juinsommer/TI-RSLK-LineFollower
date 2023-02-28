1675b1deab682577bd43e8dfed7233a2c1d2a7ea
lumpia2 <cody.cacal@yahoo.com>
2023-02-28 12:43:26 -0500

    Revised BumpInt

--- FSM_prototype/BumpInt.c
+++ FSM_prototype/BumpInt.c
@@ -49,18 +49,25 @@ policies, either expressed or implied, of the FreeBSD Project.
 #include <stdint.h>
 #include "msp.h"
 #include "../inc/Motor.h"
+
+void (*collision_handle)(uint8_t);
+
 // Initialize Bump sensors
 // Make six Port 4 pins inputs
 // Activate interface pullup
 // pins 7,6,5,3,2,0
 // Interrupt on falling edge (on touch)
 void BumpInt_Init(void(*task)(uint8_t)){
+    // Store collision handle function
+    collision_handle = task;
+
     // write this as part of Lab 14
     P4->SEL0 &= ~0xED;
     P4->SEL1 &= ~0xED;   //configure P4.0, P4.2, P4.3, P4.5, P4.6, P4.7 as GPIO
     P4->DIR &= ~0xED;    //make them inputs
     P4->REN |= 0xED;     //enable pull resistors on P4.0, P4.2, P4.3, P4.5, P4.6, P4.7
     P4->OUT |= 0xED;     //make them pull-up
+    EdgeTrigger_Init();
 }
 // Read current state of 6 switches
 // Returns a 6-bit positive logic result (0 to 63)
@@ -80,15 +87,25 @@ uint8_t Bump_Read(void){
     }
     return result; //return
 }
+
+// Enable edge triggered interrupts
+void EdgeTrigger_Init(void){
+   P4->IES |= 0xED;
+   P4->IFG &= ~0xED;
+   P4->IE |= 0xED;
+   NVIC->IP[8] = (NVIC->IP[8]&0x00FFFFFF)|0x40000000;
+   NVIC->ISER[1] = 0x00000008;
+   EnableInterrupts();
+}
+
 // we do not care about critical section/race conditions
 // triggered on touch, falling edge
 void PORT4_IRQHandler(void){
     // write this as part of Lab 14
+    P4->IFG &= ~0xED;
     uint8_t Switch; //variables to store the read data
     Switch = Bump_Read(); //read the bump switches
 
-    if(Switch != 0xED){ //if non of the switches is pressed
-        Motor_StopSimple();
-    }
+    collision_handle(Switch);
 }
 
